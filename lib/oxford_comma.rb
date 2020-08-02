ef1780ce7109cdc603f818ced1c586f4c4e4ff37
Christie Yoon <cleex1999@gmail.com>
2020-08-02 01:31:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
+#require "pry"
 def oxford_comma(array)
-
+  if array.size == 1
+  array.join
+  #binding.pry
+elsif array.size == 2
+  array.join (" and ")
+elsif array.size >= 3
+  array[-1] = "and #{array[-1]}"
+  array.join(", ")
+  end
 end
+
+
+  #else
+    #array[-1].insert(0, 'and ')
+    #return array.join(', ')
+  #end
+#end
