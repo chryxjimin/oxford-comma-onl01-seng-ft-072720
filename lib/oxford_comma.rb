e3781f7f225f0502a41c2123ab420de78912bc2e
Christie Yoon <cleex1999@gmail.com>
2020-08-02 01:28:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   array.join
   #binding.pry
 elsif array.size == 2
-  array.join ("and ")
+  array.join (" and ")
 elsif array.size == 3
   #last_word = array[-1]
   array[-1] = "and #{array[-1]}"
