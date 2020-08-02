bf581a0e9426c8b4369024a7bca10658dd9e5d00
Christie Yoon <cleex1999@gmail.com>
2020-08-02 01:25:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,10 +7,10 @@ elsif array.size == 2
   array.join ("and ")
 elsif array.size == 3
   #last_word = array[-1]
-  new_array = []
+  array = []
   #new_array = array.pop
-  new_array[-1] =  "and #{new_array[-1]}"
-  return new_array.join(", ")
+  array[-1] =  "and #{array[-1]}"
+  return array.join(", ")
   end
 end
 
