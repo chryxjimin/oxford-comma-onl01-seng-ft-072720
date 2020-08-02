3f5d59c6ba60b59bc50a6a512dcabd56bf158b4f
Christie Yoon <cleex1999@gmail.com>
2020-08-02 01:28:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,14 @@
-#require "pry"
+require "pry"
 def oxford_comma(array)
   if array.size == 1
   array.join
-  #binding.pry
+  binding.pry
 elsif array.size == 2
   array.join ("and ")
 elsif array.size == 3
   #last_word = array[-1]
-  array = []
-  #new_array = array.pop
   array[-1] =  "and #{array[-1]}"
-  return array.join(", ")
+  array.join(", ")
   end
 end
 
