39ec9c112543382f82406481858f69caa00b35af
Christie Yoon <cleex1999@gmail.com>
2020-08-02 01:13:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@
 def oxford_comma(array)
   if array.size == 1
   array.join
+  binding.pry
 elsif array.size == 2
   array.join (" and ")
-  binding.pry
 elsif array.size == 3
   #last_word = array[-1]
   new_array = [array.pop]
