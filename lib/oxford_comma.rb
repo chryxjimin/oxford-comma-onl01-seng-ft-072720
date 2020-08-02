325ccd7b10002128c5cd8c0f9021979962b758e7
Christie Yoon <cleex1999@gmail.com>
2020-08-02 01:26:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
-require "pry"
+#require "pry"
 def oxford_comma(array)
   if array.size == 1
   array.join
-  binding.pry
+  #binding.pry
 elsif array.size == 2
   array.join ("and ")
 elsif array.size == 3
