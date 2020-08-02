1ad85bc563f7c5fe8867648a50ee1014538568d0
Christie Yoon <cleex1999@gmail.com>
2020-08-02 00:29:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,14 @@ def oxford_comma(array)
   array.join
 elsif array.size == 2
   array.join (" and ")
-elsif array.size == 3
-  array.join(", ")
-  new_array = array.join(", ")
-  last_word = array.pop
-  new_array << ", and #{array.last}"
+#elsif array.size == 3
+  #array.join(", ")
+  #new_array = array.join(", ")
+  #last_word = array.pop
+  #new_array << ", and #{array.last}"
+  #end
+  else
+    array[-1].insert(0, 'and ')
+    return array.join(', ')
   end
 end
